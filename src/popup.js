e07aa80c35d641830b440f2ae52daea007d853c8
sam1037 <samngai2522@gmail.com>
2024-09-30 22:18:28 +0800

    testing chrome.storage basic usage

--- src/popup.js
+++ src/popup.js
@@ -11,14 +11,40 @@ document.addEventListener("DOMContentLoaded", function () {
 		const input = document.getElementsByClassName("blacklistInput")[0];
 		let blacklistPattern = input.value;
 		console.log(`blacklistPattern: ${blacklistPattern}`);
-		alert(`blacklistPattern: ${blacklistPattern}`);
+		//alert(`blacklistPattern: ${blacklistPattern}`);
 
 		saveBlacklistPattern(blacklistPattern);
+	});
+
+	const testBtn = document.querySelector(".testButton");
+	testBtn.addEventListener("click", function () {
+		console.log("test button clicked");
+		chrome.storage.sync.get(["testKey"]).then((result) => {
+			if (result.testKey) {
+				console.log("testKey found");
+				console.log(result.testKey);
+			} else {
+				console.log("testKey not found");
+			}
+		});
+	});
+
+	const clearBtn = document.querySelector(".clearButton");
+	clearBtn.addEventListener("click", function () {
+		console.log("clear button clicked");
+		chrome.storage.sync.clear(() => {
+			console.log("storage cleared");
+		})
 	});
 });
 
 //function to save the blacklist pattern and msg content script, para blacklistPattern should be a regex
 function saveBlacklistPattern(blacklistPattern) {
 	console.log("saveBlacklistPattern called");
-	//TODO
+
+	chrome.storage.sync.set({ "testKey": blacklistPattern }).then(() => {
+		console.log("Value is set");
+	});
+	  
+	  
 }
